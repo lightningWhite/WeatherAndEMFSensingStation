b8ccb8aa0486bab9622399e164c6c3fc104c3d23
lightningWhite <hor12020@byuui.edu>
2020-03-21 10:23:32 -0600

    Update wind_direction with more details and fixed pin connections

--- wind_direction.py
+++ wind_direction.py
@@ -3,8 +3,29 @@
 # Uses the ADC to read the voltage from the wind direction sensor
 # and maps it do the corresponding direction. It then averages the readings
 # over a period of time and then logs the average.
+#
+# Ensure the following connections:
+# 
+# Anemometer connected into the Wind Direction Sensor
+# Wind Direction Sensor connected to the RJ11 connector
+# Pin 2 on the RJ11 connector to 3v3
+# Pin 5 on the RJ11 connector to pin 1 (CH0) on the MCP3208
+# Pin 3 on the RJ11 connector to Ground (For the anemometer)
+# Pin 4 on the RJ11 connector to BCM 5 (For the anemometer)
+# Ground to pin 9 on the MCP3208 chip
+# BCM 8 (CE0) to pin 10 (CS/SHDN) on the MCP3208 chip
+# BCM 10 (MOSI) to pin 11 (Din) on the MCP3208 chip
+# BCM 9 (MISO) to pin 12 (Dout) on the MCP3208 chip
+# BCM 11 (SCLK) to pin 13 (CLK) on the MCP3208 chip
+# Ground to pin 14 (AGND) on the MCP3208 chip
+# 3v3 to pin 15 on (Vref) the MCP3208 chip
+# 3v3 to pin 16 on (Vdd) the MCP3208 chip
+# 4.7kohm resistor from ground to pin 1 on the MCP3208 chip for voltage div
 
 from gpiozero import MCP3208
+# Remove this
+from gpiozero import MCP3304
+
 import math
 import time
 
@@ -12,17 +33,18 @@ import time
 LOG_INTERVAL = 5 #900 # 15 Minutes
 
 # Analog to Digital Converter
-adc = MCP3208(channel=0)
+# adc = MCP3208(channel=0)
+adc = MCP3304(channel=0)
+
 count = 0
 values = []
 while True:
-    wind = round(adc.value * 3.3, 1)
+    # TODO: Chanbe this back to 3.3
+    wind = round(adc.value * 5.0, 1)
     if not wind in values:
         values.append(wind)
         count += 1
-        print(count)
-
-print(adc.value)
+        print(f"Count: {count}, Value: {adc.value}, Wind: {wind}")
 
 #####
 # The above code is for debugging and testing first
