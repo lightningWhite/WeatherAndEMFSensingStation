4f6f9e8c8b9e5891f9bbcdf2335eddaf9142c480
Daniel Hornberger <daniel.hornberger@aggiemail.usu.edu>
2020-03-21 12:37:24 -0600

    Monitor rainfall successfully with a log time period

--- rainfall.py
+++ rainfall.py
@@ -4,24 +4,43 @@
 #
 # Ensure the following connections to the Raspberry Pi 3 Model B:
 # 
-# Pin 2 on the RJ11 connector to Ground
+# Pin 3 on the RJ11 connector to Ground
 # Pin 4 on the RJ11 connector to BCM 6
 
 from gpiozero import Button
+import time
 
 rain_sensor = Button(6)
 BUCKET_SIZE = 0.011 # inches per bucket tip
+LOG_INTERVAL = 900 # How often to log in seconds (15 min)
 count = 0
+precipitation = 0
 
 def bucket_tipped():
     global count
+    global precipitation
     count = count + 1
-    print (count * BUCKET_SIZE)
+    precipitation = count * BUCKET_SIZE
+    print("tipped")
 
 def reset_rainfall():
     global count
     count = 0
 
+rain_sensor.when_pressed = bucket_tipped
+
 while True:
-    rain_sensor.when_pressed = bucket_tipped
+    print(precipitation)
+    time.sleep(LOG_INTERVAL)
+
+# TODO: clear the rainfall each day at midnight
+# import datetime
+# previous_day = datetime.datetime.now()
+#
+# # In loop check if the day has changed
+# current_day = datetime.datetime.now()
+# # When it's a new day, clear the rainfall total
+# if int(current_day.strftime("%w")) != int(previous_day.strftime("%w)):
+#     reset_rainfall()
+#     previous_day = current_day
 
